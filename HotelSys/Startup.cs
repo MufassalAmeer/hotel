57052d330d59da3f7f2eebf9f2ac46be432ec1b0
Mufassal Ameer <mufassal.ameer@hotmail.com>
2018-12-07 22:18:19 +0530

    Add project files.

---
+++ HotelSys/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HotelSys.Startup))]
+namespace HotelSys
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
